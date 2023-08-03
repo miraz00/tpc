ee4452364a23f1c8cf47c8d348f45b76ea84dc2a
Miraz J Naina <42742560+miraz00@users.noreply.github.com>
2023-08-03 20:54:25 +0530

    Update day4.4.c

--- day4.4.c
+++ day4.4.c
@@ -1,36 +1,35 @@
-#include<stdio.h>
-/*
-
-
-* *
- *
-* *
-
-
-*/
-int main()
-{
-    int n = 5;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j)
-            {
-                printf("*");
-            }
-            if (i + j == 5 - 1)
-            {
-                printf("%c", 8);
-            }
-            printf(" ");
-        }
-        printf("\n");
-    }
-}
-//*    *
-// *  *
-//  *
-// *  *
-//*    *
+#include<stdio.h> 
+ /* 
+  
+  
+ * * 
+  * 
+ * * 
+  
+  
+ */ 
+ int main() 
+ { 
+     int n = 5; 
+  
+     for (int i = 0; i < n; i++) 
+     { 
+         for (int j = 0; j < n; j++) 
+         { 
+             if (i == j || i + j == n - 1 ) 
+             { 
+                 printf("*"); 
+             } 
+             else
+             {
+                 printf(" "); 
+             }    
+         } 
+         printf("\n"); 
+     } 
+ } 
+ //*    * 
+ // *  * 
+ //  * 
+ // *  * 
+ //*    *
