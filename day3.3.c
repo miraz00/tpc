fe26a0cca885d7ab498b099305c0ffeabd2d5b9f
Miraz J Naina <42742560+miraz00@users.noreply.github.com>
2023-08-03 15:51:29 +0530

    Update day3.3.c

--- day3.3.c
+++ day3.3.c
@@ -1,60 +1,60 @@
-#include<stdio.h> 
- #include<string.h> 
- #include<ctype.h> 
- /* 
-   thiNk twIce cODe ONce 
-  
-   KNIHT eciwt EDOC ecno 
- */ 
- int main() 
- { 
-     char *d = "thiNk twIce cODe ONce"; 
-     int len = strlen(d); 
-     int count = 0; 
-     char s[] ="thiNk twIce cODe ONce";
-     char scp[]="thiNk twIce cODe ONce";
-     
-     for (int i = 0; i < len + 1; i++) 
-     { 
-         if (s[i] == ' ') 
-         { 
-  
-             int z = i - 1; 
-             int k; 
-             
-             for (int j = i - 1; j != -1; j--) 
-             { 
-                 k = j; 
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+/*
+thiNk twIce cODe ONce
+
+KNIHT eciwt EDOC ecno
+*/
+int main()
+{
+ char *d = "thiNk twIce cODe ONce"; //constant string literal
+ int len = strlen(d);
+ int count = 0;
+ char s[] ="thiNk twIce cODe ONce";
+ char scp[]="thiNk twIce cODe ONce";
+ printf("%s\n", s);
+ for (int i = 0; i < len + 1; i++)
+ {
+     if (s[i] == ' ' || i == len)
+     {
+
+         int z = i - 1;
+         int k;
+
+         for (int j = i - 1; j != -1; j--)
+         {
+             if(s[j] == ' ')
+             {
+                 break;
+             }
+             k = j;
+         }
+
+         if (count % 2 == 0)
+         {
+             for (int j = i - 1; j != -1 ; j--)
+             {
                  if(s[j] == ' ')
                  {
                      break;
-                 }    
-             } 
-              
-             if (count % 2 == 0) 
-             { 
-                 for (int j = i - 1; j != -1 ; j--) 
-                 { 
-                     s[k++] = toupper(scp[j]); 
-                     if(s[j] == ' ')
-                    {
-                        break;
-                    }  
-                 } 
-             } 
-             else 
-             { 
-                 for (int j = i - 1; j != -1 ; j--) 
-                 { 
-                     s[k++] = tolower(scp[j]); 
-                    if(s[j] == ' ')
-                   {
-                        break;
-                   }  
-                 } 
-             } 
-             count++;
-         } 
-     } 
-     printf("%s", s);
+                 }
+                 s[k++] = toupper(scp[j]);
+             }
+         }
+         else
+         {
+             for (int j = i - 1; j != -1 ; j--)
+             {
+                 if(s[j] == ' ')
+                 {
+                     break;
+                 }
+                 s[k++] = tolower(scp[j]);
+             }
+         }
+         count++;
+     }
  }
+ printf("%s", s);
+}
